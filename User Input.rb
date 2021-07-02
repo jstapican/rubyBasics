668b15809e986233f15b2cffb9b47326ac6dc801
jstapican <johnsteven.tapican@gmail.com>
2021-07-02 22:35:25 +0800

    Basic User Input and Output.

---
+++ User Input.rb
@@ -0,0 +1,10 @@
+# Basic Input and Output
+# puts "Enter your Name: "
+# name = gets
+# puts ("Hello " + name + ", you are awesome!")
+
+
+# Basic Input and Output - Removing the New Line Created after gets - .chomp()
+puts "Enter your Name: "
+name = gets.chomp()
+puts ("Hello " + name + ", you are awesome!")
