a6e49a9279d3aa6feadcf6379236178a1bf42995
jstapican <johnsteven.tapican@gmail.com>
2021-07-04 15:13:37 +0800

    Build a Mad Libs game where users supply certain words and thr app fits them into a phrase. This is coded using Ruby.

---
+++ Building a Ruby Mad Libs Game.rb
@@ -0,0 +1,11 @@
+# Building a Mad Libs Game using ruby.
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural_noun: "
+plural_noun = gets.chomp()
+puts "Enter a celebrity: "
+celebrity = gets.chomp()
+
+puts ("Roses are " + color + ".")
+puts (plural_noun + " are blue.")
+puts ("I love " + celebrity + ".")
