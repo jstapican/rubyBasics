0e29d2c86ea1d1c9de44dfbb30e00deb9198d1bd
jstapican <johnsteven.tapican@gmail.com>
2021-07-04 10:04:30 +0800

    Created a basic ruby calculator.

---
+++ Building a Ruby Calculator.rb
@@ -0,0 +1,30 @@
+# Basic calculator using Ruby
+# puts "Enter a number: "
+# num1 = gets.chomp()
+# puts "Enter a second number: "
+# num2 = gets.chomp()
+# puts (num1 + num2)
+
+
+# .to_i() - Converting Automatic String Types into Integers - Basic calculator using Ruby
+# puts "Enter a number: "
+# num1 = gets.chomp()
+# puts "Enter a second number: "
+# num2 = gets.chomp()
+# puts (num1.to_i + num2.to_i)
+
+
+# # .to_f() - Performing Math Operatios wih Decimals - Basic calculator using Ruby
+# puts "Enter a number: "
+# num1 = gets.chomp()
+# puts "Enter a second number: "
+# num2 = gets.chomp()
+# puts (num1.to_f + num2.to_f)
+
+
+# Combining Two Methods when we declare Variables - Basic calculator using Ruby
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+puts "Enter a second number: "
+num2 = gets.chomp().to_f
+puts (num1 + num2)
