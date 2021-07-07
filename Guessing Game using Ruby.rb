732c5456d9ae0e9bab725ca06c954bfd2ff37c1d
jstapican <johnsteven.tapican@gmail.com>
2021-07-07 21:42:43 +0800

    Building a guessing game in Ruby using While loop and IF-ELSE statements.

---
+++ Guessing Game using Ruby.rb
@@ -0,0 +1,22 @@
+# Ruby Guessing Game
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
+
+while guess != secret_word AND !out_of_guesses
+  if guess_count < guess_limit
+    puts "Enter your guess: "
+    guess = gets.chomp()
+    guess_count +=1
+  else
+    out_of_guesses = true
+  end
+end
+
+if out_of_guesses
+  puts "You lose."
+else
+  puts "You guessed it right!"
+end
