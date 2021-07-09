4feb2f9f8d9e4b856eba615b70475b2414ed0c71
jstapican <johnsteven.tapican@gmail.com>
2021-07-09 22:04:02 +0800

    Using For Loops to loop a code through a specific collection or array.

---
+++ For Loops.rb
@@ -0,0 +1,35 @@
+# For Loops
+friends = ["Tom", "Lance", "Eric", "Daryl"]
+
+for friend in friends
+  puts friend
+end
+
+
+puts
+puts
+
+# Shorthand Method - For Loops
+friends = ["Tom", "Lance", "Eric", "Daryl"]
+
+friends.each do |friend|
+  puts friend
+end
+
+
+puts
+puts
+
+# Looping through Numbers - For Loops
+for index in 0..5
+  puts index
+end
+
+
+puts
+puts
+
+# Shorthand Looping through Numbers - For Loops
+6.times do |index|
+  puts index
+end
