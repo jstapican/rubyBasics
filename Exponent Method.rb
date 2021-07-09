a0314d1d10916a65c6212c80138d477de54daa5a
jstapican <johnsteven.tapican@gmail.com>
2021-07-09 22:21:37 +0800

    Using For Loops to get the product of a number raised through its exponents.

---
+++ Exponent Method.rb
@@ -0,0 +1,12 @@
+#Exponent Method
+#This constructed example for exponent method works only for positive exponents.
+def pow(base_num, pow_num)
+  result = 1
+  pow_num.times do |index|
+    result = result * base_num
+  end
+
+  return result
+end
+
+puts pow(2,3)
