e103e2c8eea814ff5eff95442e80f7e24b7699b0
jstapican <johnsteven.tapican@gmail.com>
2021-07-09 22:37:11 +0800

    Adding Comments to your code.

---
+++ Comments.rb
@@ -0,0 +1,15 @@
+#Adding Comments to your code.
+
+# Just add a hashtag symbol before the line (#).
+
+
+# You can use multiple hastag
+# for multi-line comments.
+
+
+=begin
+
+You can use also this format when doing multi-line comments.
+Just remember that there should be no white space before the equals sign.
+
+=end
